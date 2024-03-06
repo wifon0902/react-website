216e72f05cc8b0dfe949e04dc155c538759b17b5
wifon <kaziorigor0902@gmail.com>
2024-03-07 00:15:21 +0100

    adding hero section

---
+++ src/components/HeroSection.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "../App.css";
+import { Button } from "./Button";
+import "./HeroSection.css";
+
+function HeroSection() {
+  return (
+    <div className="hero-container">
+      <h1>Hero section</h1>
+      <p>Paragraph</p>
+      <div className="hero-btns">
+        <Button
+          className="btns"
+          buttonStyle="btn--outline"
+          buttonSize="btn--large"
+        >
+          GET STARTED
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
